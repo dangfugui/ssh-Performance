3059f936fada123c91212984b83d33fb807bba18
dang <dangfugui@163.com>
2016-09-18 13:29:46 +0800

    updata

--- src/com/bctc/vo/UserKPI.java
+++ src/com/bctc/vo/UserKPI.java
@@ -3,14 +3,14 @@ package com.bctc.vo;
 import com.bctc.entity.User;
 
 public class UserKPI {
-	private User user;				//1-10 管理员    10-50中高级   50-100 中级    100+ 员工
+	private User user;					//1-10 管理员    10-50中高级   50-100 中级    100+ 员工
 	private double zuyao=0.0;			//主要负责人评分
 	private double fenguang=0.0;		//部门分管负责人评分
 	private double sijingli=0.0;		//科室经理评分
 	private double yuangong=0.0;		//员工互评
 	private double xiaji=0.0;			//下级评分
 	private double zonghe=0.0;			//综合成绩
-	private int paiming=0;			//排名
+	private int paiming=0;				//排名
 	
 	
 
